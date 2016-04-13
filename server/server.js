d5aa1ce18b269a571caf2abc53549d10b5538edc
John Hill <john@proofpositiveimaging.com>
2016-04-13 10:20:49 -0400

    cleaned up server

--- server/server.js
+++ server/server.js
@@ -4,16 +4,25 @@ var fs = require('fs');
 var app = require('./app.js');
 
 
-var httpPort = process.env.PORT || 8080;
+// var httpPort = process.env.PORT || 8080;
 
-var server = http.createServer(app).listen(httpPort);
+// var server = http.createServer(app).listen(httpPort);
 
-db.connect()
-  .then(function(conn){
-    console.log(conn.name);
-    server.listen(httpPort, function(){
-      console.log('Two Cats Server listening on port:', httpPort);
+// db.connect()
+//   .then(function(conn){
+//     console.log(conn.name);
+//     server.listen(httpPort, function(){
+//       console.log('Two Cats Server listening on port:', httpPort);
+//     });
+//   }, function(err){
+//     console.log(err);
+//   });
+
+  var port = process.env.PORT || 3000;
+  db.connect()
+  .then(function(){
+    console.log('Db is connected');
+    app.listen(port, function(){
+      console.log("Two Cats Server listening on port:" + port);
     });
-  }, function(err){
-    console.log(err);
   });
